882b1959986821eec138fa4b5bd46b72d814bf05
ayesha-offical <ayeshamughal2513@gmail.com>
2026-02-08 08:38:50 +0500

    correct url

--- phase_2/frontend/src/lib/auth.ts
+++ phase_2/frontend/src/lib/auth.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   // YAHAN BACKEND (Hugging Face) KA URL HONA CHAHIYE
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://ayeshamughal2513-focus-hub.hf.space/api/v1/auth",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://ayeshamughal2513-focus-hub.hf.space",
   fetchOptions: {
     credentials: "include", // Taake cookies cross-domain save hon
   },
